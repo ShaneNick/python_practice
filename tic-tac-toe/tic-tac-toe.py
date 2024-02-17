995d12fe413d3d74fe67ba215fdf09c07a05644c
ShaneNick <shanetomonia@gmail.com>
2024-02-16 23:13:14 -0500

    added clean board function and choosing row

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -1,7 +1,54 @@
-board = [["| | | |"],
-         ["| | | |"],
-         ["| | | |"],
-         ]
+board = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
 
-def intilize_game():
-    pass
+partially_filled_board = [
+    ["X", "O", "X"],
+    [" ", "X", "O"],
+    ["O", " ", " "]
+]
+
+#Checks to see if board is clear 
+def intilize_game(board):
+    clear_space = []
+    for row_index, row in enumerate(board):
+        if row == [" ", " ", " "]:
+            pass
+            for col_index, cell in enumerate(row):
+                if cell == " ":
+                    clear_space.append((row_index, col_index))
+
+    if clear_space:
+        print(f"Board is clear {clear_space}")
+        return True, clear_space
+    else:
+        print("No clear space found")
+        return False, []
+    
+intilize_game(board)
+
+intilize_game(partially_filled_board)
+
+def display_options():
+    print("Please choose an option (enter the number):")
+    print("1. Row 1")
+    print("2. Row 2")
+    print("3. Row 3")
+    print("4. Exit")
+
+    choice_unclean = input("Your choice: ")
+    choice_clean = choice_unclean.strip().lower()
+    return choice_clean
+
+choice_clean = display_options()
+
+if choice_clean == "row 1":
+    print('Yes')
+elif choice_clean == "row 2":
+    print('Yes')
+elif choice_clean == "row 3":
+    print('Yes')
+elif choice_clean == "row 4":
+    print('Yes')
+else:
+    print('Invalid row, please choose again')
